72bb24b2ed72ec33ef4e0895f62cb111242dae83
huytt268 <tuanhuytruong35@gmail.com>
2024-10-22 22:32:06 +0700

    Minor change

--- CleanService/Src/Modules/Booking/DTOs/BookingReturnDto.cs
+++ CleanService/Src/Modules/Booking/DTOs/BookingReturnDto.cs
@@ -1,14 +1,33 @@
 using CleanService.Src.Models;
 
-namespace CleanService.Src.Modules.Auth.DTOs;
+namespace CleanService.Src.Modules.Booking.DTOs;
 
 
 public class BookingReturnDto
 {
-    public string Id { get; set; }
-    public string UserId { get; set; }
-    public string HelperId { get; set; }
-    public DateTime BookingDate { get; set; }
-    public string BookingTime { get; set; }
-    public BookingStatus BookingStatus { get; set; }
+    public Guid Id { get; set; }
+    
+    public string CustomerId { get; set; } = null!;
+    
+    public string? HelperId { get; set; }
+    
+    public Guid ServiceId { get; set; }
+    
+    public string? Location { get; set; }
+    
+    public DateTime StartTime { get; set; }
+    
+    public DateTime? EndTime { get; set; }
+    
+    public BookingStatus Status { get; set; }
+    
+    public string? CancellationReason { get; set; }
+    
+    public decimal? Price { get; set; }
+    
+    public string? PaymentMethod { get; set; }
+    
+    public int? Rating { get; set; }
+    
+    public string? Feedback { get; set; }
 }
