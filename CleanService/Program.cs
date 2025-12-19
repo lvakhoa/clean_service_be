da0b9b7e442406f74dfdaf270baa55f49dcdd1ad
lvakhoa <22520674@gm.uit.edu.vn>
2025-12-19 11:43:51 +0700

    Add origins

--- CleanService/Program.cs
+++ CleanService/Program.cs
@@ -29,7 +29,7 @@ const string allowPolicy = "AllowCors";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(allowPolicy,
-        policy => { policy.AllowAnyOrigin().AllowCredentials().AllowAnyHeader().AllowAnyMethod(); });
+        policy => { policy.WithOrigins(origins).AllowCredentials().AllowAnyHeader().AllowAnyMethod(); });
 });
 
 builder.Logging.ClearProviders();
